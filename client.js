60fc79ae769e9a0701c0f9226d8fe42e074658f2
David Carmona <davidwalker2235@gmail.com>
2021-03-09 17:07:24 +0100

    new upload

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import {ReactInstance, Surface} from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -10,14 +10,32 @@ function init(bundle, parent, options = {}) {
     ...options,
   });
 
-  // Render your app content to the default cylinder surface
+  const balsaInfo = new Surface(
+      100,
+      100,
+      Surface.SurfaceShape.flat
+  );
+  balsaInfo.setAngle(-1.4, 0.3)
+
   r360.renderToSurface(
-    r360.createRoot('louvre_3d', { /* initial props */ }),
-    r360.getDefaultSurface()
+      r360.createRoot('infoPanel', {}),
+      balsaInfo
+  );
+
+  const matanzaInfo = new Surface(
+      100,
+      100,
+      Surface.SurfaceShape.Flat
+  );
+  matanzaInfo.setAngle(1.5, 0.2)
+
+  r360.renderToSurface(
+      r360.createRoot('infoPanel', {}),
+      matanzaInfo
   );
 
   // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+  r360.compositor.setBackground(r360.getAssetURL('louvre.jpg'));
 }
 
 window.React360 = {init};
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {
-  AppRegistry,
+  AppRegistry, asset, Image,
   StyleSheet,
   Text,
   View,
 } from 'react-360';
 
-export default class louvre_3d extends React.Component {
+export class louvre extends React.Component {
   render() {
     return (
       <View style={styles.panel}>
@@ -20,6 +20,23 @@ export default class louvre_3d extends React.Component {
   }
 };
 
+export class infoPanel extends React.Component {
+  state = {
+    img: {
+      name: 'info.png',
+      width: 100,
+      height: 100
+    }
+  }
+  render() {
+    return (
+        <View style={styles.infoPanel}>
+          <Image source={asset(`${this.state.img.name}`)} style={{width: this.state.img.width, height: this.state.img.height}} />
+        </View>
+    );
+  }
+};
+
 const styles = StyleSheet.create({
   panel: {
     // Fill the entire surface
@@ -28,6 +45,11 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(255, 255, 255, 0.4)',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  infoPanel: {
+    width: 100,
+    height: 100,
+    flexDirection: 'column',
   },
   greetingBox: {
     padding: 20,
@@ -40,4 +62,5 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('louvre_3d', () => louvre_3d);
+AppRegistry.registerComponent('louvre', () => louvre);
+AppRegistry.registerComponent('infoPanel', () => infoPanel);
